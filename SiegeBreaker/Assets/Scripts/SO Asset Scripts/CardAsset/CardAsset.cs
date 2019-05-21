06d5d58ebc479c0554390b4682342339983c7f3f
joeads <joeads@iu.edu>
2019-05-21 19:33:26 -0400

    spent way too long redesigning card and importing other assets

--- SiegeBreaker/Assets/Scripts/SO Asset Scripts/CardAsset/CardAsset.cs
+++ SiegeBreaker/Assets/Scripts/SO Asset Scripts/CardAsset/CardAsset.cs
@@ -5,9 +5,9 @@ using System.Collections.Generic;
 public enum TargetingOptions
 {
     NoTarget,
-    AllCreatures, 
-    EnemyCreatures,
-    YourCreatures, 
+    AllTroops, // Changed from AllCreatures
+    EnemyTroops, // Changed from EnemyCreatures
+    YourTroops, // Changed from  YourCreatures
     AllCharacters, 
     EnemyCharacters,
     YourCharacters
@@ -16,25 +16,25 @@ public enum TargetingOptions
 public class CardAsset : ScriptableObject 
 {
     // this object will hold the info about the most general card
-    [Header("General info")]
+    [Header("General Info")]
     public CharacterAsset characterAsset;  // if this is null, it`s a neutral card
     [TextArea(2,3)]
     public string Description;  // Description for spell or character
 	public Sprite CardImage;
     public int ManaCost;
 
-    [Header("Creature Info")]
+    [Header("Troop Info")]
     public int MaxHealth;
     public int Attack;
     public int AttacksForOneTurn = 1;
-    public bool Taunt;
-    public bool Charge;
-    public string CreatureScriptName;
-    public int specialCreatureAmount;
+    public bool Defend; // Changed from Taunt
+    public bool Mounted; // Changed from Charge
+    public string TroopScriptName; // Changed from CreatureScriptName
+    public int specialTroopAmount; // Changed from specialCreatureAmount
 
-    [Header("SpellInfo")]
-    public string SpellScriptName;
-    public int specialSpellAmount;
+    [Header("Tactic Info")]
+    public string tacticScriptName; // Changed from SpellScriptName
+    public int specialTacticAmount; // Changed from specialSpellAmount
     public TargetingOptions Targets;
 
 }
